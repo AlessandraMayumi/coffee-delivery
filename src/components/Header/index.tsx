460de3e851e49c2c4dc13aa080cfbb3f25413822
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-10-09 20:52:17 -0300

    [react-router-dom] dafeult layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/logo-coofee-delivery.svg';
+
+export function Header() {
+  return (
+    <div>
+      <a href="" target="">
+        <img src={logo} />
+      </a>
+    </div>
+  );
+}
