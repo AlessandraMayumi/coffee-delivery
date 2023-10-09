9bee8493b027aa3efabbb99b9ba109498963883d
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-10-09 20:31:36 -0300

    fix lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,41 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended'
-    ],
-    'overrides': [
-        {
-            'env': {
-                'node': true
-            },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        '@typescript-eslint',
-        'react',
-        'react-hooks',
-        'react-refresh'
-    ],
-    'rules': {
-        'react/react-in-jsx-scope': 'off',
-        'semi': ['warn', 'always'],
-        'quotes': ['warn', 'single']
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
     }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'react-refresh'
+  ],
+  'rules': {
+    'react/react-in-jsx-scope': 'off',
+    'semi': ['warn', 'always'],
+    'quotes': ['warn', 'single'],
+    'indent': ['warn', 2]
+  }
 };
